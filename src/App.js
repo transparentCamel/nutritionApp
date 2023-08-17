5a6faa45c212508924f45fac5c02e8dd7b97faf7
transparentCamel <arvydas.peciulis23@gmail.com>
2023-08-17 09:22:33 +0300

    Theme setup

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
+import { ThemeProvider } from 'styled-components';
+import { GlobalStyles } from './core/globalStyles/GlobalStyles';
+import { theme } from './core/globalStyles/Theme';
+
 function App() {
-  return <></>;
+  return (
+    <ThemeProvider theme={theme}>
+      <GlobalStyles />
+    </ThemeProvider>
+  );
 }
 
 export default App;
